b1bd4fee67396d1689a8cf78d477c9266bb03d13
kan dhillon <kdhillon469@gmail.com>
2025-05-17 15:18:40 +1000

    fix layout

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
+async function searchActive() {
+  const input = document.querySelector(".nav__input");
+  input.focus();
+  await searchClick();
+}
 
+async function searchClick() {
+  const input = document.querySelector(".nav__input");
+  const value = document.querySelector(".nav__input").value;
+  if (!value) return;
+
+  searchResult(value);
+  await Movie.renderMovies(value);
+  input.value = "";
+}
+
+async function searchBarEnter(event) {
+  let value = document.querySelector(".nav__input").value;
+  const input = document.querySelector(".nav__input");
+  //   if (!value) {
+  //     alert("please enter movie name to search");
+  //     return;
+  //   }
+  if (event.keyCode == 13) {
+    if (input === document.activeElement) {
+      searchResult(value);
+      await Movie.renderMovies(value);
+    }
+    value = document.querySelector(".movie__input").value;
+    // if (!value) {
+    //   alert("please enter movie name to search");
+    //   return;
+    // }
+    searchResult(value);
+    await Movie.renderMovies(value);
+    document.querySelector(".movie__input").value = "";
+    input.value = "";
+  }
+}
+async function searchBarClick() {
+  const input = document.querySelector(".movie__input");
+
+  if (!input.value) {
+    console.log("no value");
+    alert("please enter movie name to search");
+    return;
+  }
+  searchResult(input.value);
+  await Movie.renderMovies(input.value);
+  input.value = "";
+}
+
+function searchResult(value) {
+  if (!value) return;
+  const searchBar = document.querySelector(".movies__top");
+
+  const searchResult = document.querySelector(".movie__search--result");
+
+  const searchBarHTML = `<h2 class="movies__top--title">
+  Search results for:
+</h2>
+<h2 class="movie__search--result">"${value}"</h2>`;
+
+  searchBar.innerHTML = searchBarHTML;
+
+  searchBar.classList.add("movie__search--result-visible");
+}
--- movie.js
+++ movie.js
@@ -3,20 +3,41 @@ class Movie {
   static movies = [];
 
   static async getMovies(value) {
-    const apiUrl = `${this.apiUrl}&s=${value}`;
-    const response = await fetch(apiUrl);
-    const movieData = await response.json();
-    console.log(movieData);
+    if (!value) {
+      return;
+    }
+    try {
+      //   throw new Error("Error fetching movies");
+      const apiUrl = `${this.apiUrl}&s=${value}`;
+      const response = await fetch(apiUrl);
+      const movieData = await response.json();
+      console.log(movieData);
 
-    const movieDataArray = await movieData.Search.slice(0, 6);
-    this.movies = movieDataArray;
+      this.movies =
+        movieData.Search && movieData.Search.length > 0
+          ? movieData.Search.slice(0, 6)
+          : [];
+      console.log(this.movies);
+    } catch (error) {
+      console.log(error);
+      alert("Error fetching movies");
+    }
   }
 
-  static async renderMovies() {
+  static async renderMovies(value) {
+    const movies = document.querySelector(".movies__list");
+
+    movies.innerHTML = `<i class="fa-solid fa-spinner movies__list--loading movies__list--loading-visible"></i>
+    `;
+    await this.getMovies(value);
+    if (!this.movies.length) {
+      movies.innerHTML = `<p class="movies__list--no-movies">No movies found</p>`;
+      return;
+    }
     const moviesHTML = this.movies
       .map(
         (movie) => `
-        <div class="movie movie__invisible">
+        <div class="movie ">
         <figure class="movie__img--wrapper">
         <img src="${movie.Poster}" alt="" class="movie__img">
         <h3 class="movie__info--title">${movie.Title}</h3>
@@ -39,10 +60,6 @@ class Movie {
         </div>`
       )
       .join("");
-    const movies = document.querySelector(".movies__list");
-
-    movies.innerHTML =
-      `<i class="fa-solid fa-spinner movies__list--loading movies__list--loading-visible"></i>
-` + moviesHTML;
+    movies.innerHTML = moviesHTML;
   }
 }
